f682c70627da48f0bbb725f022a45dc0dac878f0
subhendu20 <64896535+subhendu20@users.noreply.github.com>
2023-09-27 12:11:32 +0530

    new

--- client/src/components/Note.js
+++ client/src/components/Note.js
@@ -115,7 +115,7 @@ function Note(props) {
                                         {note.title}
 
                               </div>
-                              <div className="date" >Due date- {note.toISOString().split('T')[0]}
+                              <div className="date" >Due date- {note.date.split('T')[0]}
                               </div>
                               <div className="description">
                                         {note.description}
